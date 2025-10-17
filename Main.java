749cef2d7a1d639ead180f9a40fdc659f57acc5b
KevvyN8 <kyonn.ooh@gmail.com>
2025-10-17 21:46:31 +0000

    tried

--- Main.java
+++ Main.java
@@ -5,5 +5,41 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    Scanner sc = new Scanner(System.in)
+    System.out.println("Enter a # to add. Enter -1 to stop");
+    int stop = 0;
+    int sum = 0;
+
+    while (stop != -1)
+    {
+      stop = sc.nextInt();
+      sum += stop;
+    }
+    stop++;
+    System.out.println("The sum is: " +N);
+
+    sc.close();
+
+
+
+    //problem 2
+
+
+
+  System.out.println("How many #'s do you wish to input");
+
+  int index = 0
+  num = sc.nextInt();
+
+  while (index < num)
+  {
+    
+  }
+
+    
+
+
+
+
   }
 }
